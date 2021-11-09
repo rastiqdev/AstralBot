bcadb4905fffd37c697a22903f2c3d68337d051c
Louis Licari <louis.licari91@gmail.com>
2021-11-09 08:17:30 +0100

    welcome image

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,52 +1,9 @@
-const {createCanvas} = require("canvas");
-const Canvas = require("canvas");
-const {MessageAttachment, MessageEmbed} = require("discord.js");
+const sendWelcomeImage = require('../functions/sendWelcomeImage'); 
 module.exports = {
     name: "guildMemberAdd",
     async execute(client, member) {
 
-        const canvas = createCanvas(900, 300)
-        const ctx = canvas.getContext("2d")
+        sendWelcomeImage(client, member)
 
-        const img = await Canvas.loadImage(__dirname + "/../../res/welcome_image.png")
-        ctx.drawImage(img, 0, 0, 900, 300)
-
-        let avatar_url = member.avatarURL({ size: 300, format: "png" })
-        if (!avatar_url) avatar_url = member.user.defaultAvatarURL
-        const avatar = await Canvas.loadImage(member.user.avatarURL({ size: 300, format: "png" }))
-
-        const x = 600
-        const y = 0
-        const w = 300
-        const h = 300
-        const radius = 150
-
-        ctx.save();
-
-        ctx.beginPath();
-        ctx.moveTo(x + radius, y);
-        ctx.lineTo(x + w - radius, y);
-        ctx.quadraticCurveTo(x + w, y, x + w, y + radius);
-        ctx.lineTo(x + w, y + h - radius);
-        ctx.quadraticCurveTo(x + w, y + h, x + w - radius, y + h);
-        ctx.lineTo(x + radius, y + h);
-        ctx.quadraticCurveTo(x, y + h, x, y + h - radius);
-        ctx.lineTo(x, y + radius);
-        ctx.quadraticCurveTo(x, y, x + radius, y);
-        ctx.closePath();
-
-        ctx.clip();
-        ctx.drawImage(avatar, x, y, w, h);
-        ctx.restore();
-
-        const attachment = new MessageAttachment(canvas.toBuffer(), "welcome_img.png")
-        const embed = new MessageEmbed()
-            .setTitle(`${member.user.tag} a rejoint le serveur !`)
-            .setDescription("🎉 Bienvenue à toi ! 🎉")
-            .setImage("attachment://welcome_img.png")
-            .setColor("#0099ff")
-            .setFooter("On est maintenant " + member.guild.memberCount + " membres !")
-        const channel = await member.guild.channels.fetch(client.config.welcomeChannelId)
-        await channel.send({ embeds: [embed], files: [attachment]})
     }
 }
---
+++ src/functions/sendWelcomeImage.js
@@ -0,0 +1,43 @@
+const Jimp = require('jimp');
+const path = require('path');
+const {MessageAttachment, MessageEmbed} = require("discord.js");
+
+
+module.exports = sendWelcomeImage = async function(client, member) {
+
+    try {
+        const canvas = new Jimp(500, 150);
+        const avatar = await Jimp.read(member.user.displayAvatarURL({format: 'png'}));
+  
+        const Quantify_55_white = await Jimp.loadFont(path.join(__dirname, '../../res/fonts/Quantify_55_white.fnt'));
+        const Quantify_25_white = await Jimp.loadFont(path.join(__dirname, '../../res/fonts/Quantify_25_white.fnt'));
+        const OpenSans_22_white = await Jimp.loadFont(path.join(__dirname, '../../res/fonts/OpenSans_22_white.fnt'));
+        const mask = await Jimp.read('https://raw.githubusercontent.com/CoderDixs/DraftBot/master/images/avatarMask.png');
+  
+        avatar.resize(136, Jimp.AUTO);
+        mask.resize(136, Jimp.AUTO);
+        avatar.mask(mask, 0, 0);
+  
+        canvas.blit(avatar, 5, 5);
+  
+        canvas.print(Quantify_55_white, 158, 20, 'Bienvenue');
+        canvas.print(OpenSans_22_white, 158, 70, 'sur le serveur Discord');
+        canvas.print(Quantify_25_white, 158, 105, 'Le Nid d\'Astral');
+  
+        const buffer = await canvas.getBufferAsync(Jimp.MIME_PNG);
+        const attachment = new MessageAttachment(buffer, 'joinimg.png');
+  
+        const embed = new MessageEmbed()
+        .setTitle(`${member.user.tag} a rejoint le serveur !`)
+        .setDescription("🎉 Bienvenue à toi ! 🎉")
+        .setImage("attachment://joinimg.png")
+        .setColor("#0099ff")
+        .setFooter("On est maintenant " + member.guild.memberCount + " membres !")
+        
+        const channel = await member.guild.channels.fetch(client.config.welcomeChannelId)
+        await channel.send({ embeds: [embed], files: [attachment]})
+      } catch (error) {
+        return console.log('WelcomeImage => ',error);
+      }
+
+}
