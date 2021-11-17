3c13870f9fdc76cd65c41f75f2c28574c0bf8adc
Leo21 <leo.kos.mars@gmail.com>
2021-11-17 20:47:55 +0100

    add kick and joins logs

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,9 +1,18 @@
-const sendWelcomeImage = require('../functions/sendWelcomeImage'); 
+const sendWelcomeImage = require('../functions/sendWelcomeImage');
+const {MessageEmbed} = require("discord.js");
 module.exports = {
     name: "guildMemberAdd",
     async execute(client, member) {
 
-        sendWelcomeImage(client, member)
+        const embed = new MessageEmbed()
+            .setAuthor("Un membre a rejoint le serveur")
+            .setColor("#0099ff")
+            .setTimestamp(Date.now())
+            .setThumbnail(member.avatarURL({ dynamic: true }))
+            .addField("Utilisateur :", `${member.id}`, true)
+        const channel = await member.guild.channels.fetch(client.config.logs.joinsChannelId)
+        await channel.send({embeds: [embed]})
 
+        await sendWelcomeImage(client, member)
     }
 }
