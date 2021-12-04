99f77b9287d9377841c1bfa2ef4081c3f57ef9ff
Louis Licari <louis.licari91@gmail.com>
2021-12-04 19:04:43 +0100

    put youtube sub count in function

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 const {MessageEmbed} = require("discord.js");
 const cron = require("node-cron")
-const { google } = require("googleapis")
+const updateSubCount = require('../functions/updateSubCount');
 
 module.exports = {
     name: "ready",
@@ -26,30 +26,11 @@ module.exports = {
             "WATCHING"
         ]
 
-        const youtube = google.youtube("v3")
-        const subChannel = await (
-            await client.guilds.fetch(client.config.mainGuildId)).channels.fetch(client.config.subCounterChannelId)
-        const viewsChannel = await (
-            await client.guilds.fetch(client.config.mainGuildId)).channels.fetch(client.config.viewsCounterChannelId)
+
 
         // Repeat every 20 minutes
         cron.schedule("*\20 * * * *", async () => {
-            const res = await youtube.channels.list({
-                auth: process.env.YOUTUBE_API_KEY,
-                id: client.config.astralYoutubeChannelId,
-                part: ["statistics"]
-            })
-
-            const statistics = res.data.items[0].statistics
-
-            await subChannel.setName(`Abonnés : ${statistics.subscriberCount} ${statistics.subscriberCount.includes("69") ? "(nice)" : ""}`)
-            await viewsChannel.setName(`Total de vues : ${statistics.viewCount} ${statistics.videoCount.includes("69") ? "(nice)" : ""}`)
-            // await subChannel.setTopic(`${statistics.subscriberCount} abonnés ` +
-            //     `${statistics.subscriberCount.includes("69") ? "(nice)" : ""}, ${statistics.videoCount} vidéos ` +
-            //     `${statistics.videoCount.includes("69") ? "(nice)" : ""} et un total de ${statistics.viewCount} ` +
-            //     `${statistics.viewCount.includes("69") ? "(nice)" : ""} vues !`)
-
-            client.logger.debug("Updated the subscribers counter !")
+            updateSubCount(client)
         })
 
         const setPresence = function(number) {
