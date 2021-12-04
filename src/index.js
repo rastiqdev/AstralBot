414e63f9213b4f67e8089687adbe61b2f097a4dc
Leo21 <leo.kos.mars@gmail.com>
2021-12-04 19:02:15 +0100

    re-oups

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ const client = new Client({
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_BANS,
-        Intents.FLAGS.DIRECT_MESSAGES
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
     ],
     partials: ["CHANNEL", "MESSAGE"]
 });
